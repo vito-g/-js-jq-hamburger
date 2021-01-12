045fd4e38c81057fe27fe70fb296e3ffb3e04d74
Vito Gentile <gentilevito1076@gmail.com>
2021-01-12 16:11:16 +0100

    Correzione sui commenti

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
 //Devo applicare la funzione show alla classe hamburger-menu
 // $('.hamburger-menu').show();
 // Ma solo al passaggio del mouse
-//Dunque dovrò inserirla all'interno dell'action hover assieme alla funzione hide
+//Dunque dovrò inserirla all'interno di una action click. Farò lo stesso con la action hide
 
-
-// $('.header-right > a').click(
-//     function() {
-//     $('.hamburger-menu').show('slow');
-//   },
-//     function() {
-//     $('.hamburger-menu').hide('slow');
-//   });
 $('.header-right > a').click(
     function() {
     $('.hamburger-menu').show('slow');
