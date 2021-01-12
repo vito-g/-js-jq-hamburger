d1b95962cc1d9db693f73b74d5ab4a319e884c97
Vito Gentile <gentilevito1076@gmail.com>
2021-01-12 19:09:35 +0100

    Altri interventi con JQuery

--- js/main.js
+++ js/main.js
@@ -10,7 +10,39 @@ $('.header-right > a').click(
     function() {
     $('.hamburger-menu').show('slow');
   });
+
 $('a.close').click(
     function() {
     $('.hamburger-menu').hide('slow');
   });
+// --------------------------------------------------------------------------------
+// Altre trasformazioni: Colori & Dimensioni
+
+//Change Boolean Logo Dimension on over
+$('.header-left > img').hover(
+  function() {
+    $(this).css('height','41px');
+  },
+  function() {
+    $(this).css('height','40px');
+  });
+
+// Change Hamburger-menu  Color on over
+$('.header-right > a').hover(
+  function() {
+  $(this).css('color','#00e165');
+  },
+  function() {
+  $(this).css('color','lightgrey');
+});
+
+//Change Hamburger-menu List Item Color & Font-Weight
+$('.hamburger-menu ul li a').hover(
+  function() {
+  $(this).css("color", "#00e165");
+  $(this).css("font-weight", "600");
+  },
+  function() {
+  $(this).css("color", "white");
+  $(this).css("font-weight", "normal");
+});
