bda4cc69d6b47019e99e927817a68faac7900600
Vito Gentile <gentilevito1076@gmail.com>
2021-01-12 15:21:58 +0100

    ACtion Show & hide inserite tramite una funzione nell' action hover

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
-//Devo applicare la funzione slow alla classe hamburger-menu
-$('.hamburger-menu').show();
+//Devo applicare la funzione show alla classe hamburger-menu
+// $('.hamburger-menu').show();
+// Ma solo al passaggio del mouse
+//Dunque dovrò inserirla all'interno dell'action hover assieme alla funzione hide
+
+
+$('a').hover(
+    function() {
+    $('.hamburger-menu').show('slow');
+  },
+    function() {
+    $('.hamburger-menu').hide('slow');
+  });
