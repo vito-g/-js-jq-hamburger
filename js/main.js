3ac7b1df95b302076f3572b84720d4f792caf396
Vito Gentile <gentilevito1076@gmail.com>
2021-01-12 16:03:33 +0100

    Action decommentata

--- js/main.js
+++ js/main.js
@@ -4,14 +4,14 @@
 //Dunque dovrò inserirla all'interno dell'action hover assieme alla funzione hide
 
 
-$('.header-right > a').click(
-    function() {
-    $('.hamburger-menu').show('slow');
-  },
-    function() {
-    $('.hamburger-menu').hide('slow');
-  });
 // $('.header-right > a').click(
 //     function() {
 //     $('.hamburger-menu').show('slow');
+//   },
+//     function() {
+//     $('.hamburger-menu').hide('slow');
 //   });
+$('.header-right > a').click(
+    function() {
+    $('.hamburger-menu').show('slow');
+  });
